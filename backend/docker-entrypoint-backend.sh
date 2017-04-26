997490a5789f4542c063e3c27532b37643fbbe78
Alex Scherbanov <alex@egotv.ru>
2017-04-26 14:12:16 +0300

    configure backend entrypoint to rerun make on every up

--- backend/docker-entrypoint-backend.sh
+++ backend/docker-entrypoint-backend.sh
@@ -2,7 +2,7 @@
 
 if [[ -z "$*" ]]; then
     cp ~/secrets/dot.erlang.cookie ~/.erlang.cookie && \
-        # make clean app rel && \
+        make clean app rel && \
         ~/theproject/_rel/hello_world_example/bin/hello_world_example foreground
 else
     "$@"
