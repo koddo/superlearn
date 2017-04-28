b1dbeefdd7a57bce0199d688b491ce9b90552d18
Alex Scherbanov <alex@egotv.ru>
2017-04-28 14:13:10 +0300

    add recompile_handler.sh to see if compile:file() would work with erl_call()

--- backend/recompile_handler.sh
+++ backend/recompile_handler.sh
@@ -7,5 +7,8 @@ compile:file(\"/home/theuser/theproject/src/$HANDLER\", [{outdir, \"/home/theuse
 code:load_abs(\"/home/theuser/theproject/ebin/$HANDLER\").
 "
 
+# error_logger:info_msg(\"--- recompiled handler with result: ~p~n~n~n~n\", [Result]).
+
+
 echo $CMD | erl_call -c hello_world_example -name hello_world_example@$(hostname) -e
 echo
