d2a1ce760926b7bd3fa8b425677c8c3f6187afd4
Alex Scherbanov <alex@egotv.ru>
2017-05-08 17:58:04 +0300

    add core.match dependency

--- client-web/project.clj
+++ client-web/project.clj
@@ -14,6 +14,7 @@
                  [com.cognitect/transit-cljs "0.8.239"]
                  [day8.re-frame/http-fx "0.1.3"]
                  [com.andrewmcveigh/cljs-time "0.4.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  ]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
