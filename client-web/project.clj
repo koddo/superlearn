8302511d815689a6f9629bd37ac469bdf386fd35
Alex Scherbanov <alex@egotv.ru>
2017-05-18 20:15:16 +0300

    make path to js absolute

--- client-web/project.clj
+++ client-web/project.clj
@@ -57,7 +57,7 @@
      :compiler     {:main                 theproject.core
                     :output-to            "resources/public/js/compiled/app.js"
                     :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
+                    :asset-path           "/js/compiled/out"
                     :source-map-timestamp true
                     :preloads             [devtools.preload]
                     :external-config      {:devtools/config {:features-to-install :all}}
