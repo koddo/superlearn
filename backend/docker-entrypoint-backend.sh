70d01eb84dc2591fd2d55f8ff9dc26409d7f3beb
Alex Scherbanov <alex@egotv.ru>
2017-10-19 20:53:55 +0300

    erlang cookie must be with permissions 700

--- backend/docker-entrypoint-backend.sh
+++ backend/docker-entrypoint-backend.sh
@@ -2,6 +2,7 @@
 
 if [[ -z "$*" ]]; then
     cp ~/secrets/dot.erlang.cookie ~/.erlang.cookie && \
+        chmod 700 ~/.erlang.cookie && \
         make clean app rel && \
         ~/theproject/_rel/hello_world_example/bin/hello_world_example foreground
 else
