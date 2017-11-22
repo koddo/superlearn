f0664924948b99a8697dc5aea8318eb0da8a40e4
Alex Scherbanov <alex@egotv.ru>
2017-11-22 20:42:20 +0300

    make render_dtl also response to post requests and output debug info

--- backend/src/handler_render_dtl.erl
+++ backend/src/handler_render_dtl.erl
@@ -1,17 +1,34 @@
 -module(handler_render_dtl).
 
 -export([init/2]).
+-export([allowed_methods/2]).
 -export([content_types_provided/2]).
+-export([content_types_accepted/2]).
 -export([render_dtl/2]).
+-export([handle_post/2]).
 
 init(Req, Opts) ->
 	{cowboy_rest, Req, Opts}.
+
+allowed_methods(Req, State) ->
+	{[<<"HEAD">>, <<"GET">>, <<"POST">>, <<"OPTIONS">>], Req, State}.
 
 content_types_provided(Req, State = {_Module, MediaType, _RenderArgs}) ->
 	{[
       {MediaType, render_dtl}
      ], Req, State}.
 
+content_types_accepted(Req, State) ->
+	{[{{<<"application">>, <<"x-www-form-urlencoded">>, []}, handle_post}],
+     Req, State}.
+
 render_dtl(Req, State = {Module, _MediaType, RenderArgs}) ->
+    error_logger:info_msg("--- render_dtl get~n--- Bindings: ~p~n", [cowboy_req:bindings(Req)]),
     {ok, Body} = apply(Module, render, [RenderArgs]),
     {Body, Req, State}.
+
+handle_post(Req, State) ->
+	{ok, BodyPost, Req1} = cowboy_req:read_urlencoded_body(Req),
+    error_logger:info_msg("--- render_dtl post~n--- Bindings: ~p~n--- BodyPost: ~p~n", [cowboy_req:bindings(Req1), BodyPost]),
+    {true, Req1, State}.
+
--- backend/src/hello_world_app.erl
+++ backend/src/hello_world_app.erl
@@ -38,6 +38,7 @@ dispatch() ->
                                   {"/ht/cards/add", handler_render_dtl, {cards_add_html_dtl, <<"text/html">>, #{}}},
                                   {"/ht/cards/[:card_id]", handler_cards, []},
                                   {"/ht/decks", handler_render_dtl, {decks_html_dtl, <<"text/html">>, #{}}},
+                                  {"/ht/review/:card_id/:response", handler_render_dtl, {card_review_html_dtl, <<"text/html">>, #{}}},
                                   {"/ht/asdf", handler_asdf, []}
                                   %% {"/ht/cards/:card_id", handler_render_dtl, {card_html_dtl, <<"text/html">>, #{front => <<"what">>, back => <<"ever">>, due => {{2017,10,24},{0,0,0}}}}},
                                   %% {"/ht/cards", handler_cards, []}
