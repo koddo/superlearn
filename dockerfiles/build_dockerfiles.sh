dcccccb0e5289f945dea68cc3fd250d8cada2880
Alex Scherbanov <alex@egotv.ru>
2017-11-22 20:00:33 +0300

    move old dockerfiles somewhere for a while

--- dockerfiles/build_dockerfiles.sh
+++ dockerfiles/build_dockerfiles.sh
@@ -7,7 +7,7 @@ find . -type f -name "*.m4" -print0 | while IFS= read -r -d $'\0' FILE_M4; do
     DOCKERFILE="Dockerfile_$STRIPPED"
     echo "$FILE_M4 -> $DOCKERFILE"
     m4 "$FILE_M4" > "$DOCKERFILE"             # maybe --include=includes_dir/
-    # docker build --tag "koddo/$STRIPPED" - < "$DOCKERFILE"
+    docker build --tag "koddo/$STRIPPED" - < "$DOCKERFILE"
 done
 
 
